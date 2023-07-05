0261d9ebd45a96bc9f5df998f84a6faf26ba3de2
ATUL TINGRE <atultingre.work@gmail.com>
2023-07-06 02:37:36 +0530

    MUI Rating

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MuiTextFiled from "./components/MuiTextFiled";
 import MuiRadioButton from "./components/MuiRadioButton";
 import MuiCheckbox from "./components/MuiCheckbox";
 import MuiSwitch from "./components/MuiSwitch";
+import MuiRating from "./components/MuiRating";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <MuiRadioButton />
       <MuiCheckbox />
       <MuiSwitch/>
+      <MuiRating/>
     </div>
   );
 }
---
+++ src/components/MuiRating.js
@@ -0,0 +1,212 @@
+import React, { useState } from "react";
+import { Rating, Stack } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
+const MuiRating = () => {
+  const [value, setValue] = useState(null);
+  const [dValue, setDValue] = useState(3);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleDChange = (e) => {
+    setDValue(e.target.value);
+  };
+  console.log({ value });
+  return (
+    <Stack spacing={4}>
+      <h1>Mui Rating</h1>
+      <Stack>
+        <Rating
+          value={value}
+          onChange={handleChange}
+          precision={0.5}
+          size="small"
+        />
+        <Rating
+          value={value}
+          onChange={handleChange}
+          precision={0.5}
+          size="medium"
+        />
+        <Rating
+          value={value}
+          onChange={handleChange}
+          precision={0.5}
+          size="large"
+        />
+      </Stack>
+
+      <Stack>
+        <Rating
+          value={value}
+          onChange={handleChange}
+          precision={0.5}
+          size="medium"
+          icon={<FavoriteIcon fonstSize="inherit"/>}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        />
+        <Rating
+          value={value}
+          onChange={handleChange}
+          precision={0.5}
+          size="medium"
+          icon={<FavoriteIcon fonstSize="inherit" color="secondary" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        />
+        <Rating
+          value={value}
+          onChange={handleChange}
+          precision={0.5}
+          size="medium"
+          icon={<FavoriteIcon fonstSize="inherit" color="error" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        />
+        <Rating
+          value={value}
+          onChange={handleChange}
+          precision={0.5}
+          size="medium"
+          icon={<FavoriteIcon fonstSize="inherit" color="warning"/>}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        />
+        <Rating
+          value={value}
+          onChange={handleChange}
+          precision={0.5}
+          size="medium"
+          icon={<FavoriteIcon fonstSize="inherit" color="success"/>}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        />
+        <Rating
+          value={value}
+          onChange={handleChange}
+          precision={0.5}
+          size="medium"
+          icon={<FavoriteIcon fonstSize="inherit" color="info"/>}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        />
+      </Stack>
+
+      <Stack>
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" readOnly />}
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="secondary"/>}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" readOnly />}
+          color="secondary"
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="warning"/>}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" readOnly />}
+          color="error"
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="error"/>}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" readOnly />}
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="success"/>}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" readOnly />}
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="info"/>}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" readOnly />}
+        />
+      </Stack>
+
+      <Stack>
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" />}
+          emptyIcon={
+            <FavoriteBorderIcon fontSize="inherit"  />
+          } highlightSelectedOnly
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="secondary"/>}
+          emptyIcon={
+            <FavoriteBorderIcon fontSize="inherit"  />
+          } highlightSelectedOnly
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="warning"/>}
+          emptyIcon={
+            <FavoriteBorderIcon fontSize="inherit"  />
+          } highlightSelectedOnly
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="error"/>}
+          emptyIcon={
+            <FavoriteBorderIcon fontSize="inherit"  />
+          } highlightSelectedOnly
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="success"/>}
+          emptyIcon={
+            <FavoriteBorderIcon fontSize="inherit"  />
+          } highlightSelectedOnly
+        />
+        <Rating
+          value={dValue}
+          onChange={handleDChange}
+          precision={0.5}
+          size="large"
+          icon={<FavoriteIcon fonstSize="inherit" color="info"/>}
+          emptyIcon={
+            <FavoriteBorderIcon fontSize="inherit"  />
+          } highlightSelectedOnly
+        />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiRating;
